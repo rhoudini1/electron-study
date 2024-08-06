09af8cceb64229db4b7c57ffc61b3a42b5f0ebe9
Renan <renanmonteiro@pecege.com>
2024-08-06 17:48:47 -0300

    add save text functionality to save button

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 const electron = require("electron");
-const { app, BrowserWindow } = electron;
+const fs = require("fs");
+const path = require("node:path");
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on("ready", () => {
-  const mainWindow = new BrowserWindow();
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
   mainWindow.loadFile("index.html");
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
+
+ipcMain.on("save", (event, text) => {
+  // todo: popup to show save dialog window
+  fs.writeFile("text.txt", text, (err) => {
+    if (err) console.log("Error saving file.", err);
+  });
 });
