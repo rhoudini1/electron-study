82965ee0f746a6dcf0f0d4224bd5af87f1ebd84a
Renan Monteiro <araujomonteirogba@hotmail.com>
2024-08-19 09:34:02 -0300

    fix: add more fields to news

--- 4-news-reader/js/index.js
+++ 4-news-reader/js/index.js
@@ -7,11 +7,11 @@ function showNews(allNews) {
   allNews.forEach((news) => {
     const singleNews = `
       <li class="list-group-item">
-        <img class="img-circle media-object pull-left" src="" width="50" height="50" />
+        <img class="img-circle media-object pull-left" src="${news.urlToImage}" width="50" height="50" />
         <div class="media-body">
           <strong><a href="">${news.title}</a></strong>
           <div>
-            <span class="">21st June 2020</span>
+            <span class="">${news.publishedAt}</span>
             <span class="pull-right">Author: ${news.author}</span>
           </div>
           <p>${news.description}</p>
